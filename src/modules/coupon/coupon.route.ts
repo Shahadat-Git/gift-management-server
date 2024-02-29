bd0df31b14196e13e81de568f1b87d629d20f17f
Shahadat-Git <noyon560@gmail.com>
2024-02-29 15:31:56 +0600

    auth permission added for coupon route

--- src/modules/coupon/coupon.route.ts
+++ src/modules/coupon/coupon.route.ts
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import { couponControllers } from "./coupon.controller";
+import auth from "../../middlewares/auth";
 
 const router = Router();
 
 // create coupon route
-router.post("/create-coupon", couponControllers.createCoupon);
+router.post("/create-coupon", auth("manager"), couponControllers.createCoupon);
 
 // get all coupon
-router.get("/coupons", couponControllers.getAllCoupons);
-// get coupon
+router.get("/coupons", auth("manager"), couponControllers.getAllCoupons);
 
+// get coupon
 router.get("/coupon/:id", couponControllers.getCoupon);
 
 export const couponRoutes = router;
